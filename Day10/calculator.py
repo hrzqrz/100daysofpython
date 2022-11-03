eae0515022569bc2e069020c8de40f7d2e351804
hrzqrz <hrzqrz@gmail.com>
2022-11-03 15:13:32 +0330

    file calculator was modified and updated to better performance

--- Day10/calculator.py
+++ Day10/calculator.py
@@ -29,7 +29,16 @@ operation_symbol = input('Pick an operation from the line above: ')
 num2 = int(input('Whats the second number : '))
 
 calculate_function = operations[operation_symbol]
-result = calculate_function(num1, num2)
+first_answer = calculate_function(num1, num2)
 
-print(f"{num1} {operation_symbol} {num2} = {result}")
+print(f"{num1} {operation_symbol} {num2} = {first_answer}")
+
+operation_symbol = input('Pich another operation: ')
+
+num3 = int(input('What is the next number?: '))
+
+calculate_function = operations[operation_symbol]
+second_ansdwer = calculate_function(first_answer, num3)
+
+print(f"{first_answer} {operation_symbol} {num3} = {second_ansdwer}")
  
